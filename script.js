560b1320895339e8f9846edd90ce77a9e07fb752
ES <sadiqzadee001@gmail.com>
2025-04-17 15:08:37 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Burger Menu
+const burger = document.getElementById('burger');
+const navLinks = document.querySelector('.nav-links');
+
+burger.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
+
+// Smooth Scrolling
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
